4344027ad8179e61f5844df46fd11641858ccef3
Looqer <lukaszsienkiewicz94@gmail.com>
2020-05-17 22:59:04 +0200

    dictionary almost done

--- one/DictionaryClass.java
+++ one/DictionaryClass.java
@@ -1,14 +1,11 @@
 package one;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class DictionaryClass extends Thread {
 
     List<Word> myDictionary = new ArrayList<>();
-    int duplicaterate, onlyinonefile;
+    int duplicaterate, onlyinonefile, frequency;
 
     @Override
     public void run() {
@@ -24,19 +21,22 @@ public class DictionaryClass extends Thread {
             Set<String> hashsetFiles = new HashSet<String>();
             Set<String> hashsetUnique = new HashSet<String>();
             Set<String> hashsetNUnique = new HashSet<String>();
+            Word[] frequencyRank = new Word[5];
 
             duplicaterate = 0;
             onlyinonefile = 0;
+            frequency = 0;
             for(Word wordex : myDictionary){
-                duplicaterate = 0;
+                duplicaterate = frequency = 0;
                 hashsetFiles.add(wordex.sourceFile);
                 //System.out.println("ogarniamy slowo " + wordex.wordvalue + " z " + wordex.sourceFile);
                 for (Word wordexa : myDictionary){
                     //System.out.println("ze slowem " + wordexa.wordvalue + " z " + wordexa.sourceFile);
-                    if(!wordex.sourceFile.equals(wordexa.sourceFile)){
-                        if(wordex.wordvalue.equals(wordexa.wordvalue)){
+                    if(wordex.wordvalue.equals(wordexa.wordvalue)){
+                        if(!wordex.sourceFile.equals(wordexa.sourceFile)){
                             duplicaterate++;
                         }
+                        frequency++;
                     }
                 }
                 if(duplicaterate == 0){
@@ -45,7 +45,24 @@ public class DictionaryClass extends Thread {
                 else{
                     hashsetNUnique.add(wordex.wordvalue);
                 }
+
+                wordex.occurences = frequency;
+                if ((frequencyRank.equals(null))){
+                    frequencyRank[0] = wordex;
+                }
+
+                if ((wordex.occurences > frequencyRank[0].occurences)){
+                    frequencyRank[0] = wordex;
+                }
+                for(int i = 0; i < 4; i++){
+                    if (frequencyRank[i].occurences > frequencyRank[i+1].occurences){
+                        frequencyRank[i+1] = frequencyRank[i];
+                    }
+                }
             }
+
+            System.out.println("rankng " + Arrays.toString(frequencyRank));
+
             System.out.println("Plikow w slowniku: " + hashsetFiles.size());
             hashsetFiles.clear();
             System.out.println("Slow co sa tylko w jednym pliku: " + hashsetUnique.size());
--- one/Word.java
+++ one/Word.java
@@ -2,4 +2,5 @@ package one;
 
 public class Word {
     String sourceFile, wordvalue;
+    int occurences;
 }
